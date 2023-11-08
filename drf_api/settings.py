af5c0599172f4348505c602c8af1155105eb919f
TylerMorgan142 <tylermorgan142@gmail.com>
2023-11-08 04:49:32 +0000

    fixed indent errors in settings.py

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -47,10 +47,10 @@ REST_AUTH_SERIALIZERS = {
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
- SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
- DEBUG = 'DEV' in os.environ
+DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = ['localhost', 'herokuapp.com']
 
